3c9f32defe032be18d97cbfd1996fb1fbfaa4c1a
20221tiimi0033 <marialiramoreira7@gmail.com>
2024-06-07 09:11:49 -0300

    galeria

--- app/src/main/java/moreira/lira/galeria/MainActivity.java
+++ app/src/main/java/moreira/lira/galeria/MainActivity.java
@@ -1,9 +1,13 @@
 package moreira.lira.galeria;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -35,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void setSupportActionBar(Toolbar toolbar) {
 
+    }
+    @Override
+    //criando as opções do menu definidas no arquivo/adiciona as definições no menu da activity
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+    @Override
+    //toda vez que o item no toolbar for selecionado sera executado o codigo que abre a camera
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+        case R.id.opCamera:
+            dispatchTakePictureIntent();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/moreira/lira/galeria/PhotoActivity.java
+++ app/src/main/java/moreira/lira/galeria/PhotoActivity.java
@@ -1,9 +1,13 @@
 package moreira.lira.galeria;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -32,4 +36,26 @@ public class PhotoActivity extends AppCompatActivity {
 
     private void setSupportActionBar(Toolbar toolbar) {
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super .onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void sharePhoto() {
+    }
+} 
